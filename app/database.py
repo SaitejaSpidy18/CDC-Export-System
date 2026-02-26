5510e7a17d7aacae092a1cb947aa9ef05bcef012
Sai Teja <saitejaphani18@gmail.com>
2026-02-26 09:59:11 +0530

    Add SQLAlchemy database session setup

---
+++ app/database.py
@@ -0,0 +1,19 @@
+# app/database.py
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=False, future=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
